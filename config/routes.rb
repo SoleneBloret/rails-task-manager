600be6219f04650b2ede697b31b2781c80241d6e
Solene Bloret <solene.bloret@hotmail.fr>
2026-05-19 16:37:38 +0200

    Part 8 Refactorisation upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   get "tasks", to: "tasks#index"
+#   get "tasks/new", to: "tasks#new"
+#   get "tasks/:id", to: "tasks#show", as: "task"
+#   get "tasks/:id/edit", to: "tasks#edit", as: "taskeditor"
+#   post "tasks", to: "tasks#create"
+#   delete "tasks/:id", to: "tasks#destroy"
+#   patch "tasks/:id", to: "tasks#update"
+
+
+#   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+#   # Can be used by load balancers and uptime monitors to verify that the app is live.
+#   get "up" => "rails/health#show", as: :rails_health_check
+
+#   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+#   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+#   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
+#   # Defines the root path route ("/")
+#   # root "posts#index"
+# end
+
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "tasks", to: "tasks#index"
-  get "tasks/new", to: "tasks#new"
-  get "tasks/:id", to: "tasks#show", as: "task"
-  get "tasks/:id/edit", to: "tasks#edit", as: "taskeditor"
-  post "tasks", to: "tasks#create"
-  delete "tasks/:id", to: "tasks#destroy"
-  patch "tasks/:id", to: "tasks#update"
-
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  resources :tasks
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
